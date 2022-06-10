80743341d7096ab2e6f61af503f333ec7ad1263c
Carl Benedict Garces <carlgarces7@gmail.com>
2022-06-10 19:45:23 +0800

    tried changing the logo.. didnt work

--- ggy_twitter_clone/lib/src/screens/navProfilePage.dart
+++ ggy_twitter_clone/lib/src/screens/navProfilePage.dart
@@ -13,171 +13,177 @@ class navProfilePage extends StatefulWidget {
 
 class _profileState extends State<navProfilePage> {
   final AuthController _auth = locator<AuthController>();
+  final ScrollController _scProfile = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child:
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        SizedBox(
-          height: 50,
-          width: 200,
-          child: Center(
-            child: Column(
-              children: [
-                Text('Profile',
-                    style: GoogleFonts.quicksand(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: const Color(0xff7b7b7b),
-                    )),
-              ],
+        body: SingleChildScrollView(
+      controller: _scProfile,
+      child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const SizedBox(height: 50),
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: Center(
+              child: Column(
+                children: [
+                  Text('Profile',
+                      style: GoogleFonts.quicksand(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xff7b7b7b),
+                      )),
+                ],
+              ),
             ),
           ),
-        ),
-        Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(15),
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onLongPress: () {
+                      _selectImgDialogue();
+                    },
+                    child: AvatarImage(
+                      uid: FirebaseAuth.instance.currentUser!.uid,
+                      radius: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    // width: double.infinity,
+                    width: 15,
+                    height: 15,
+                  ),
+                  UserNameFromDB(
+                      uid: FirebaseAuth.instance.currentUser!.uid,
+                      fontSize: 25),
+                  EmailFromDB(
+                      uid: FirebaseAuth.instance.currentUser!.uid,
+                      fontSize: 10),
+                ],
+              ),
+            ),
           ),
-          child: Center(
+          SizedBox(
+            height: 350,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                InkWell(
-                  onLongPress: () {
+                ElevatedButton(
+                  onPressed: () {
                     _selectImgDialogue();
                   },
-                  child: AvatarImage(
-                    uid: FirebaseAuth.instance.currentUser!.uid,
-                    radius: 40,
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    fixedSize: const Size(240, 50),
+                    primary: Colors.lightBlueAccent,
+                    // : Theme.of(context).colorScheme.onSurface),
+                  ),
+                  child: Text(
+                    'Change Profile Picture',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
                   ),
                 ),
-                const SizedBox(
-                  // width: double.infinity,
-                  width: 15,
-                  height: 15,
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    fixedSize: const Size(240, 50),
+                    primary: Colors.lightBlueAccent,
+                    // : Theme.of(context).colorScheme.onSurface),
+                  ),
+                  child: Text(
+                    'Change Username',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
                 ),
-                UserNameFromDB(
-                    uid: FirebaseAuth.instance.currentUser!.uid, fontSize: 25),
-                EmailFromDB(
-                    uid: FirebaseAuth.instance.currentUser!.uid, fontSize: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    fixedSize: const Size(240, 50),
+                    primary: Colors.lightBlueAccent,
+                    // : Theme.of(context).colorScheme.onSurface),
+                  ),
+                  child: Text(
+                    'Change Email',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    fixedSize: const Size(240, 50),
+                    primary: Colors.blue,
+                    // : Theme.of(context).colorScheme.onSurface),
+                  ),
+                  child: Text(
+                    'Change Password',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _logoutDialogue();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    fixedSize: const Size(240, 50),
+                    primary: Colors.red,
+                    // : Theme.of(context).colorScheme.onSurface),
+                  ),
+                  child: Text(
+                    'Log Out',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-        ),
-        SizedBox(
-        height: 350,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  _selectImgDialogue();
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  fixedSize: const Size(240, 50),
-                  primary: Colors.lightBlueAccent,
-                  // : Theme.of(context).colorScheme.onSurface),
-                ),
-                child: Text(
-                  'Change Profile Picture',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  fixedSize: const Size(240, 50),
-                  primary: Colors.lightBlueAccent,
-                  // : Theme.of(context).colorScheme.onSurface),
-                ),
-                child: Text(
-                  'Change Username',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  fixedSize: const Size(240, 50),
-                  primary: Colors.lightBlueAccent,
-                  // : Theme.of(context).colorScheme.onSurface),
-                ),
-                child: Text(
-                  'Change Email',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  fixedSize: const Size(240, 50),
-                  primary: Colors.blue,
-                  // : Theme.of(context).colorScheme.onSurface),
-                ),
-                child: Text(
-                  'Change Password',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _logoutDialogue();
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  fixedSize: const Size(240, 50),
-                  primary: Colors.red,
-                  // : Theme.of(context).colorScheme.onSurface),
-                ),
-                child: Text(
-                  'Log Out',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ]),
+        ]),
+      ),
     ));
   }
 
@@ -214,6 +220,7 @@ class _profileState extends State<navProfilePage> {
       },
     );
   }
+
   Future<void> _selectImgDialogue() async {
     return showDialog<void>(
       context: context,
@@ -224,8 +231,16 @@ class _profileState extends State<navProfilePage> {
           content: SingleChildScrollView(
             child: Column(
               children: [
-                TextButton(onPressed: (){ImageService.updateProfileImagegallery();},child: const Text('Select Image From Galley')),
-                TextButton(onPressed: (){ImageService.updateProfileImagecamera();},child: const Text('Select Image From Camera')),
+                TextButton(
+                    onPressed: () {
+                      ImageService.updateProfileImagegallery();
+                    },
+                    child: const Text('Select Image From Galley')),
+                TextButton(
+                    onPressed: () {
+                      ImageService.updateProfileImagecamera();
+                    },
+                    child: const Text('Select Image From Camera')),
               ],
             ),
           ),
